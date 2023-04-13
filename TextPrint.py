ad8116611385706254a15e344ddc232187a3c499
Adam Howard <achdisposable1@gmail.com>
2023-04-13 17:48:27 +0100

    clean up gek a bit

---
+++ TextPrint.py
@@ -0,0 +1,24 @@
+import pygame
+from common import BLACK
+
+
+class TextPrint(object):
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 20)
+
+    def tprint(self, screen, textString):
+        textBitmap = self.font.render(textString, True, BLACK)
+        screen.blit(textBitmap, (self.x, self.y))
+        self.y += self.line_height
+
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+
+    def indent(self):
+        self.x += 10
+
+    def unindent(self):
+        self.x -= 10
--- gek.py
+++ gek.py
@@ -2,36 +2,17 @@
 
 import pygame
 import math
-
+from common import *
 
 # Define some colors.
-BLACK = pygame.Color('black')
-WHITE = pygame.Color('white')
+from TextPrint import TextPrint
+
+
 
 
 # This is a simple class that will help us print to the screen.
 # It has nothing to do with the joysticks, just outputting the
 # information.
-class TextPrint(object):
-    def __init__(self):
-        self.reset()
-        self.font = pygame.font.Font(None, 20)
-
-    def tprint(self, screen, textString):
-        textBitmap = self.font.render(textString, True, BLACK)
-        screen.blit(textBitmap, (self.x, self.y))
-        self.y += self.line_height
-
-    def reset(self):
-        self.x = 10
-        self.y = 10
-        self.line_height = 15
-
-    def indent(self):
-        self.x += 10
-
-    def unindent(self):
-        self.x -= 10
 
 
 pygame.init()
@@ -39,7 +20,7 @@ pygame.init()
 # Set the width and height of the screen (width, height).
 screen = pygame.display.set_mode((500, 900))
 
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Controller Test")
 
 # Loop until the user clicks the close button.
 done = False
@@ -52,45 +33,6 @@ pygame.joystick.init()
 
 # Get ready to print.
 textPrint = TextPrint()
-
-def getAngleFromXY(XAxisValue, YAxisValue):
-
-    #Normally Atan2 takes Y,X, not X,Y.  We switch these around since we want 0
-    # degrees to be straight up, not to the right like the unit circle;
-    angleInRadians = math.atan2(XAxisValue, YAxisValue * -1)
-
-    #Atan2 gives us a negative value for angles in the 3rd and 4th quadrants.
-    # We want a full 360 degrees, so we will add 2 PI to negative values.
-    if angleInRadians < 0.0:
-        angleInRadians = angleInRadians + (math.pi * 2.0)
-
-    #Convert the radians to degrees.  Degrees are easier to visualize.
-    angleInDegrees = 180.0 * angleInRadians / math.pi
-
-    return angleInDegrees
-
-def convertXYtoDirection(X, Y):
-    sectors = 12
-    #We have 8 sectors, so get the size of each in degrees.
-    sectorSize = 360.0 / sectors
-
-    #We also need the size of half a sector
-    halfSectorSize = sectorSize / 2.0
-
-    #First, get the angle using the function above
-    thumbstickAngle = getAngleFromXY(X, Y)
-
-    #Next, rotate our angle to match the offset of our sectors.
-    convertedAngle = thumbstickAngle + halfSectorSize
-
-    #Finally, we get the current direction by dividing the angle
-    # by the size of the sectors
-    direction = math.floor(convertedAngle / sectorSize)
-
-    #the result directions map as follows:
-    # 0 = UP, 1 = UP-RIGHT, 2 = RIGHT ... 7 = UP-LEFT.
-    return direction % sectors
-
 
 # -------- Main Program Loop -----------
 while not done:
